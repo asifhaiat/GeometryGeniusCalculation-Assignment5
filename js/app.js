40eeda009d0fb90ae7978594f3e072a6c9496d0f
asifhaiat <asifhaiat674652gmail.com>
2023-03-21 17:58:03 +0600

    validate all input field

--- js/app.js
+++ js/app.js
@@ -14,28 +14,34 @@ document.getElementById("calculateTriangleButton").addEventListener("click", fun
   //   console.log(triangleHeightString);
   const triangleHeightNumber = parseFloat(triangleHeightString.value);
   // console.log(triangleHeightNumber);
-  const initialTriangleValue = 0.5;
-  const totalTriangleArea = (initialTriangleValue * triangleBaseNumber * triangleHeightNumber).toFixed(2);
-  //   console.log(totalTriangleArea);
-
-  /* Set Total Value in textarea field */
-  const textAreaTriangleString = document.getElementById("areaCalculation");
-  textAreaTriangleString.value = totalTriangleArea;
 
   // Clear the input fields
   triangleBaseString.value = "";
   triangleHeightString.value = "";
 
-  // Get the element whose background color I want to change
-  const triangleBackgroundColor = document.querySelector(".bg-rose-200");
+  if (isNaN(triangleBaseNumber) || isNaN(triangleHeightNumber) || triangleBaseNumber < 0 || triangleHeightNumber < 0) {
+    alert("Please input only positive values for triangle base and height.");
+    return;
+  } else {
+    const initialTriangleValue = 0.5;
+    const totalTriangleArea = (initialTriangleValue * triangleBaseNumber * triangleHeightNumber).toFixed(2);
+    //   console.log(totalTriangleArea);
 
-  // Generate a random color value using the Math.random() function
-  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
-    Math.random() * 256
-  )})`;
+    /* Set Total Value in textarea field */
+    const textAreaTriangleString = document.getElementById("areaCalculation");
+    textAreaTriangleString.value = totalTriangleArea;
 
-  // Set the background color of the element to the random color value
-  triangleBackgroundColor.style.backgroundColor = randomColor;
+    // Get the element whose background color I want to change
+    const triangleBackgroundColor = document.querySelector(".bg-rose-200");
+
+    // Generate a random color value using the Math.random() function
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )})`;
+
+    // Set the background color of the element to the random color value
+    triangleBackgroundColor.style.backgroundColor = randomColor;
+  }
 });
 
 /* Rectangle Calculate */
@@ -49,27 +55,36 @@ document.getElementById("calculateRectangleButton").addEventListener("click", fu
   //   console.log(rectangleLengthString);
   const rectangleLengthNumber = parseFloat(rectangleLengthString.value);
   //   console.log(rectangleLengthNumber);
-  const totalRectangleArea = (rectangleWidthNumber * rectangleLengthNumber).toFixed(2);
-  //   console.log(totalRectangleArea);
-
-  /* Set Total Value in textarea field */
-  const textAreaRectangleString = document.getElementById("areaCalculation");
-  textAreaRectangleString.value = totalRectangleArea;
 
   // Clear the input fields
   rectangleWidthString.value = "";
   rectangleLengthString.value = "";
 
-  // Get the element whose background color I want to change
-  const rectangleBackgroundColor = document.querySelector(".bg-orange-400");
+  if (
+    isNaN(rectangleWidthNumber) ||
+    isNaN(rectangleLengthNumber) ||
+    rectangleWidthNumber < 0 ||
+    rectangleLengthNumber < 0
+  ) {
+    alert("Please input only positive values for Rectangle width and length.");
+    return;
+  } else {
+    const totalRectangleArea = (rectangleWidthNumber * rectangleLengthNumber).toFixed(2);
+    //   console.log(totalRectangleArea);
 
-  // Generate a random color value using the Math.random() function
-  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
-    Math.random() * 256
-  )})`;
+    /* Set Total Value in textarea field */
+    const textAreaRectangleString = document.getElementById("areaCalculation");
+    textAreaRectangleString.value = totalRectangleArea;
 
-  // Set the background color of the element to the random color value
-  rectangleBackgroundColor.style.backgroundColor = randomColor;
+    // Get the element whose background color I want to change
+    const rectangleBackgroundColor = document.querySelector(".bg-orange-400");
+    // Generate a random color value using the Math.random() function
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )})`;
+    // Set the background color of the element to the random color value
+    rectangleBackgroundColor.style.backgroundColor = randomColor;
+  }
 });
 
 /* Parallelogram Calculate */
@@ -83,27 +98,38 @@ document.getElementById("calculateParallelogramButton").addEventListener("click"
   // console.log(parallelogramHeightString);
   const parallelogramHeightNumber = parseFloat(parallelogramHeightString.value);
   //   console.log(parallelogramHeightNumber);
-  const totalParallelogramArea = (parallelogramBaseNumber * parallelogramHeightNumber).toFixed(2);
-  //   console.log(totalParallelogramArea);
-
-  /* Set Total Value in textarea field */
-  const textAreaParallelogramString = document.getElementById("areaCalculation");
-  textAreaParallelogramString.value = totalParallelogramArea;
 
   // Clear the input fields
   parallelogramBaseString.value = "";
   parallelogramHeightString.value = "";
 
-  // Get the element whose background color I want to change
-  const parallelogramBackgroundColor = document.querySelector(".bg-yellow-400");
+  if (
+    isNaN(parallelogramBaseNumber) ||
+    isNaN(parallelogramHeightNumber) ||
+    parallelogramBaseNumber < 0 ||
+    parallelogramHeightNumber < 0
+  ) {
+    alert("Please input only positive values for Parallelogram base and height.");
+    return;
+  } else {
+    const totalParallelogramArea = (parallelogramBaseNumber * parallelogramHeightNumber).toFixed(2);
+    //   console.log(totalParallelogramArea);
 
-  // Generate a random color value using the Math.random() function
-  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
-    Math.random() * 256
-  )})`;
+    /* Set Total Value in textarea field */
+    const textAreaParallelogramString = document.getElementById("areaCalculation");
+    textAreaParallelogramString.value = totalParallelogramArea;
 
-  // Set the background color of the element to the random color value
-  parallelogramBackgroundColor.style.backgroundColor = randomColor;
+    // Get the element whose background color I want to change
+    const parallelogramBackgroundColor = document.querySelector(".bg-yellow-400");
+
+    // Generate a random color value using the Math.random() function
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )})`;
+
+    // Set the background color of the element to the random color value
+    parallelogramBackgroundColor.style.backgroundColor = randomColor;
+  }
 });
 
 /* Rhombus Calculate */
@@ -117,28 +143,34 @@ document.getElementById("calculateRhombusButton").addEventListener("click", func
   // console.log(parallelogramHeightString);
   const rhombusHeightNumber = parseFloat(rhombusHeightString.value);
   //   console.log(parallelogramHeightNumber);
-  const initialRhombusValue = 0.5;
-  const totalRhombusArea = (initialRhombusValue * rhombusBaseNumber * rhombusHeightNumber).toFixed(2);
-  // console.log(totalRhombusArea);
-
-  /* Set Total Value in textarea field */
-  const textAreaRhombusString = document.getElementById("areaCalculation");
-  textAreaRhombusString.value = totalRhombusArea;
 
   // Clear the input fields
   rhombusBaseString.value = "";
   rhombusHeightString.value = "";
 
-  // Get the element whose background color I want to change
-  const rhombusBackgroundColor = document.querySelector(".bg-gray-200");
+  if (isNaN(rhombusBaseNumber) || isNaN(rhombusHeightNumber) || rhombusBaseNumber < 0 || rhombusHeightNumber < 0) {
+    alert("Please input only positive values for Rhombus base and height.");
+    return;
+  } else {
+    const initialRhombusValue = 0.5;
+    const totalRhombusArea = (initialRhombusValue * rhombusBaseNumber * rhombusHeightNumber).toFixed(2);
+    // console.log(totalRhombusArea);
 
-  // Generate a random color value using the Math.random() function
-  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
-    Math.random() * 256
-  )})`;
+    /* Set Total Value in textarea field */
+    const textAreaRhombusString = document.getElementById("areaCalculation");
+    textAreaRhombusString.value = totalRhombusArea;
 
-  // Set the background color of the element to the random color value
-  rhombusBackgroundColor.style.backgroundColor = randomColor;
+    // Get the element whose background color I want to change
+    const rhombusBackgroundColor = document.querySelector(".bg-gray-200");
+
+    // Generate a random color value using the Math.random() function
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )})`;
+
+    // Set the background color of the element to the random color value
+    rhombusBackgroundColor.style.backgroundColor = randomColor;
+  }
 });
 
 /* Pentagon Calculate */
@@ -152,27 +184,34 @@ document.getElementById("calculatePentagonButton").addEventListener("click", fun
   // console.log(parallelogramHeightString);
   const pentagonHeightNumber = parseFloat(pentagonHeightString.value);
   //   console.log(parallelogramHeightNumber);
-  const initialPentagonValue = 0.5;
-  const totalPentagonArea = (initialPentagonValue * pentagonBaseNumber * pentagonHeightNumber).toFixed(2);
-  //   console.log(totalPentagonArea);
 
-  /* Set Total Calculated Value in textarea */
-  const pentagonTextAreaString = document.getElementById("areaCalculation");
-  pentagonTextAreaString.value = totalPentagonArea;
   //   Clear The Input Field
   pentagonBaseString.value = "";
   pentagonHeightString.value = "";
 
-  // Get the element whose background color I want to change
-  const pentagonBackgroundColor = document.querySelector(".bg-purple-200");
+  if (isNaN(pentagonBaseNumber) || isNaN(pentagonHeightNumber) || pentagonBaseNumber < 0 || pentagonHeightNumber < 0) {
+    alert("Please input only positive values for Pentagon base and height.");
+    return;
+  } else {
+    const initialPentagonValue = 0.5;
+    const totalPentagonArea = (initialPentagonValue * pentagonBaseNumber * pentagonHeightNumber).toFixed(2);
+    //   console.log(totalPentagonArea);
 
-  // Generate a random color value using the Math.random() function
-  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
-    Math.random() * 256
-  )})`;
+    /* Set Total Calculated Value in textarea */
+    const pentagonTextAreaString = document.getElementById("areaCalculation");
+    pentagonTextAreaString.value = totalPentagonArea;
 
-  // Set the background color of the element to the random color value
-  pentagonBackgroundColor.style.backgroundColor = randomColor;
+    // Get the element whose background color I want to change
+    const pentagonBackgroundColor = document.querySelector(".bg-purple-200");
+
+    // Generate a random color value using the Math.random() function
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )})`;
+
+    // Set the background color of the element to the random color value
+    pentagonBackgroundColor.style.backgroundColor = randomColor;
+  }
 });
 
 /* Ellipse Calculate */
@@ -186,25 +225,32 @@ document.getElementById("calculateEllipseButton").addEventListener("click", func
   // console.log(parallelogramHeightString);
   const ellipseBAxisNumber = parseFloat(ellipseBAxisString.value);
   //   console.log(parallelogramHeightNumber);
-  const initialEllipseValue = 3.14;
-  const totalEllipseArea = (initialEllipseValue * ellipseAAxisNumber * ellipseBAxisNumber).toFixed(2);
-  //   console.log(totalEllipseArea);
 
-  /* Set Total Calculated Value in textarea */
-  const ellipseTextAreaString = document.getElementById("areaCalculation");
-  ellipseTextAreaString.value = totalEllipseArea;
   //   Clear The Input Field
   ellipseAAxisString.value = "";
   ellipseBAxisString.value = "";
 
-  // Get the element whose background color I want to change
-  const ellipseBackgroundColor = document.querySelector(".bg-lime-100");
+  if (isNaN(ellipseAAxisNumber) || isNaN(ellipseBAxisNumber) || ellipseAAxisNumber < 0 || ellipseBAxisNumber < 0) {
+    alert("Please input only positive values for Ellipse A axis and B axis.");
+    return;
+  } else {
+    const initialEllipseValue = 3.14;
+    const totalEllipseArea = (initialEllipseValue * ellipseAAxisNumber * ellipseBAxisNumber).toFixed(2);
+    //   console.log(totalEllipseArea);
 
-  // Generate a random color value using the Math.random() function
-  const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
-    Math.random() * 256
-  )})`;
+    /* Set Total Calculated Value in textarea */
+    const ellipseTextAreaString = document.getElementById("areaCalculation");
+    ellipseTextAreaString.value = totalEllipseArea;
 
-  // Set the background color of the element to the random color value
-  ellipseBackgroundColor.style.backgroundColor = randomColor;
+    // Get the element whose background color I want to change
+    const ellipseBackgroundColor = document.querySelector(".bg-lime-100");
+
+    // Generate a random color value using the Math.random() function
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )})`;
+
+    // Set the background color of the element to the random color value
+    ellipseBackgroundColor.style.backgroundColor = randomColor;
+  }
 });
